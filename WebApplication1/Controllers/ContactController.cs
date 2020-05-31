46d3492e07355e3cd3015f26153adb30ede06585
mpanuska <56010445+mpanuska@users.noreply.github.com>
2020-05-31 14:14:32 -0500

    contact form controllers added

---
+++ WebApplication1/Controllers/ContactController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(WebApplication1.Models.EmailFormModel model)
+        {
+            string SenderEmail = System.Configuration.ConfigurationManager.AppSettings["SenderEmail"].ToString();
+            string SenderPassword = System.Configuration.ConfigurationManager.AppSettings["SenderPassword"].ToString();
+
+            MailMessage mailMessage = new MailMessage(SenderEmail, model.Email, model.Subject, "Thank you for reaching out to Matts Boxing regarding: " + model.Body + "We will be contacting you shortly");
+            mailMessage.IsBodyHtml = false;
+            mailMessage.BodyEncoding = UTF8Encoding.UTF8;
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.Timeout = 100000;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential(SenderEmail, SenderPassword);
+            client.Send(mailMessage);
+            ViewBag.Message = "Form submitted";
+
+
+            return View();
+        }
+
+
+    }
+}
---
+++ WebApplication1/Models/EmailFormModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class EmailFormModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string From { get; set; }
+        public string Subject { get; set; }
+        
+        [Required]
+        [Display(Name = "Body")]
+        public string Body { get; set; }
+    }
+}
